ce5c6a2fd089a4f1cfd9d2ab786cb56b93b09e79
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-18 15:04:59 -0400

    Created room description styling [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[D[D[D[D[D[D[D[D[Dindividual room hero and banner styling

--- src/components/Rooms/SingleRoom.js
+++ src/components/Rooms/SingleRoom.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import defaultBcg from "../../images/room-1.jpeg";
+import { useSelector } from "react-redux";
+import Hero from "../Hero/Hero";
+import Banner from "../Banner/Banner";
 
-const SingleRoom = () => {
+const SingleRoom = ({ match }) => {
+  const state = useSelector((state) => state);
+  const rooms = state.hotel.rooms;
+  const slug = match.params.slug;
+
+  const getRoom = (slug) => {
+    const room = rooms.find((room) => room.fields.slug === slug);
+    return room;
+  };
+
+  const room = getRoom(slug);
+  console.log(room);
+
+  if (!room) {
+    return (
+      <div className="error">
+        <h3>no such room could be found</h3>
+        <Link to="/rooms" className="btn-primary">
+          back to rooms
+        </Link>
+      </div>
+    );
+  }
+
+  const {
+    name,
+    description,
+    capacity,
+    size,
+    prize,
+    extras,
+    breakfast,
+    pets,
+    images,
+  } = room.fields;
   return (
-    <div>
-      <h1>Single Room</h1>
-    </div>
+    <Hero hero="roomsHero">
+      <Banner title={`${name} room`}>
+        <Link to="/rooms" className="btn-primary">
+          back to rooms
+        </Link>
+      </Banner>
+    </Hero>
   );
 };
 
