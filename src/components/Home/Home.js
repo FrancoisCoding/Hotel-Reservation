49b55bb9a53b580164e4b40038ae5cb77fc95acf
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-18 13:39:48 -0400

    Added banner and hero components to error, rooms, and home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Hero from "../Hero/Hero";
+import Banner from "../Banner/Banner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Hero from "../Hero/Hero";
+import { Link } from "react-router-dom";
+import Banner from "../Banner/Banner";
 
 const Rooms = () => {
   return (
     <div>
-      <Hero hero="roomsHero" />
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
