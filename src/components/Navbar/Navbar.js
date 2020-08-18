3e5e3f506533df1c7c8f96a582b1e90280b533d7
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-18 13:24:56 -0400

    Created and styled navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../images/logo.svg";
-import { FaAlignRight } from "react-icons";
+import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [navbarState, setNavbarState] = useState({ isOpen: false });
+
+  const handleToggle = () => {
+    setNavbarState({ isOpen: !navbarState.isOpen });
+  };
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Beach Resort" />
+          </Link>
+          <button type="button" className="nav-btn" onClick={handleToggle}>
+            <FaAlignRight className="nav-icon"></FaAlignRight>
+          </button>
+        </div>
+        <ul className={navbarState.isOpen ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
