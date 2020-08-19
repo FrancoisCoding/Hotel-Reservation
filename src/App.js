89b0ed063fec052835f5e9d3dd1a0950faeea341
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-19 00:53:41 -0400

    Connected to contentful to receive rooms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Routes from "./components/Routes/Routes";
 import Navbar from "./components/Navbar/Navbar";
+import Client from "./components/Contentful/Contentful";
+
+Client.getEntries().then((res) => console.log(res.items));
 
 function App() {
   return (
